80edbba408d4ee6cf2bc610d657ef691b1e18940
rdb <git@rdb.name>
2019-11-02 17:23:51 +0100

    Indicate that Sphinx 1.8 is required

--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ from sphinx_interrogatedb import idb
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.8'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
