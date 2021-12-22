87eba1d30739d3eb3b355375a3cb4e9659061a80
rdb <git@rdb.name>
2021-12-22 20:18:50 +0100

    Fix some issues with sample on "using intervals" part of Hello World

--- introduction/tutorial/using-intervals-to-move-the-panda.py
+++ introduction/tutorial/using-intervals-to-move-the-panda.py
@@ -4,6 +4,7 @@ from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
+from direct.interval.IntervalGlobal import LerpPosInterval, LerpHprInterval
 from panda3d.core import Point3
 
 
@@ -27,32 +28,27 @@ class MyApp(ShowBase):
 
         # Load and transform the panda actor.
         self.panda_actor = Actor("models/panda-model",
-                                {"walk": "models/panda-walk4"})
+                                 {"walk": "models/panda-walk4"})
         self.panda_actor.set_scale(0.005, 0.005, 0.005)
         self.panda_actor.reparent_to(self.render)
         # Loop its animation.
         self.panda_actor.loop("walk")
 
-        # Create the four lerp intervals needed for the panda to
+        # Create the sequence of lerp intervals needed for the panda to
         # walk back and forth.
-        pos_ival1 = self.panda_actor.posInterval(13,
-                                                 Point3(0, -10, 0),
-                                                 startPos=Point3(0, 10, 0))
-        pos_ival2 = self.panda_actor.posInterval(13,
-                                                 Point3(0, 10, 0),
-                                                 startPos=Point3(0, -10, 0))
-        hpr_ival1 = self.panda_actor.hprInterval(3,
-                                                 Point3(180, 0, 0),
-                                                 startHpr=Point3(0, 0, 0))
-        hpr_ival2 = self.panda_actor.hprInterval(3,
-                                                 Point3(0, 0, 0),
-                                                 startHpr=Point3(180, 0, 0))
+        self.panda_pace = Sequence(
+            LerpPosInterval(self.panda_actor, 13,
+                            Point3(0, -10, 0), startPos=Point3(0, 10, 0)),
+            LerpHprInterval(self.panda_actor, 3,
+                            Point3(180, 0, 0), startHpr=Point3(0, 0, 0)),
 
-        # Create and play the sequence that coordinates the intervals.
-        self.pandaPace = Sequence(pos_ival1, hpr_ival1,
-                                  pos_ival2, hpr_ival2,
-                                  name="pandaPace")
-        self.pandaPace.loop()
+            LerpPosInterval(self.panda_actor, 13,
+                            Point3(0, 10, 0), startPos=Point3(0, -10, 0)),
+            LerpHprInterval(self.panda_actor, 3,
+                            Point3(0, 0, 0), startHpr=Point3(180, 0, 0)),
+            name="panda-pace"
+        )
+        self.panda_pace.loop()
 
     # Define a procedure to move the camera.
     def spin_camera_task(self, task):
